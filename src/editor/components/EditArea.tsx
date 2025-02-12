9906e598b47c38720002e51838574585889553cf
XieZongChen <952937570@qq.com>
2025-02-12 11:02:04 +0800

    feat: 添加 react-dnd

--- src/editor/components/EditArea.tsx
+++ src/editor/components/EditArea.tsx
@@ -6,30 +6,6 @@ export function EditArea() {
   const { components, addComponent } = useComponentsStore();
   const { componentConfig } = useComponentConfigStore();
 
-  useEffect(() => {
-    addComponent(
-      {
-        id: 222,
-        name: 'Container',
-        props: {},
-        children: [],
-      },
-      1
-    );
-
-    addComponent(
-      {
-        id: 333,
-        name: 'Button',
-        props: {
-          text: '无敌',
-        },
-        children: [],
-      },
-      222
-    );
-  }, []);
-
   function renderComponents(components: Component[]): React.ReactNode {
     return components.map((component: Component) => {
       const config = componentConfig?.[component.name];
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom/client';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
 import App from './App.tsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // react-dnd 用来跨组件传递数据的 provider
+  <DndProvider backend={HTML5Backend}>
+    <App />
+  </DndProvider>
+);
