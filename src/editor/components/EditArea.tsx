1b2cae99384dc6e75097dd80a65b0195c8f5c0d7
XieZongChen <952937570@qq.com>
2025-02-12 10:59:29 +0800

    feat: 组件渲染

--- src/editor/components/EditArea.tsx
+++ src/editor/components/EditArea.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from 'react';
-import { useComponentsStore } from '../stores/components';
+import React, { useEffect } from 'react';
+import { Component, useComponentsStore } from '../stores/components';
+import { useComponentConfigStore } from '../stores/component-config';
 
 export function EditArea() {
   const { components, addComponent } = useComponentsStore();
+  const { componentConfig } = useComponentConfigStore();
 
   useEffect(() => {
     addComponent(
@@ -14,11 +16,45 @@ export function EditArea() {
       },
       1
     );
+
+    addComponent(
+      {
+        id: 333,
+        name: 'Button',
+        props: {
+          text: '无敌',
+        },
+        children: [],
+      },
+      222
+    );
   }, []);
 
+  function renderComponents(components: Component[]): React.ReactNode {
+    return components.map((component: Component) => {
+      const config = componentConfig?.[component.name];
+
+      if (!config?.component) {
+        return null;
+      }
+
+      return React.createElement(
+        config.component,
+        {
+          key: component.id,
+          ...config.defaultProps,
+          ...component.props,
+        },
+        // components 是一个树形结构，这里要递归渲染 children
+        renderComponents(component.children || [])
+      );
+    });
+  }
+
   return (
-    <div>
-      <pre>{JSON.stringify(components, null, 2)}</pre>
+    <div className='h-[100%]'>
+      {/* <pre>{JSON.stringify(components, null, 2)}</pre> */}
+      {renderComponents(components)}
     </div>
   );
 }
--- src/editor/stores/component-config.tsx
+++ src/editor/stores/component-config.tsx
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 import Container from '../materials/Container';
 import Button from '../materials/Button';
+import Page from '../materials/Page';
 
 export interface ComponentConfig {
   name: string;
@@ -31,6 +32,11 @@ export const useComponentConfigStore = create<State & Action>((set) => ({
         text: '按钮',
       },
       component: Button,
+    },
+    Page: {
+      name: 'Page',
+      defaultProps: {},
+      component: Page,
     },
   },
   registerComponent: (name, componentConfig) =>
