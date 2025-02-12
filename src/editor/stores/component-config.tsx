afeaec7d01de232cd113f2a6ee26e0d987fd239d
XieZongChen <952937570@qq.com>
2025-02-12 10:54:41 +0800

    feat: 组件配置 store

---
+++ src/editor/stores/component-config.tsx
@@ -0,0 +1,46 @@
+import { create } from 'zustand';
+import Container from '../materials/Container';
+import Button from '../materials/Button';
+
+export interface ComponentConfig {
+  name: string;
+  defaultProps: Record<string, any>; // 组件默认参数
+  component: any; // 组件实例
+}
+
+interface State {
+  // componentConfig 的映射，key 是组件名，value 是组件配置
+  componentConfig: { [key: string]: ComponentConfig };
+}
+
+interface Action {
+  registerComponent: (name: string, componentConfig: ComponentConfig) => void;
+}
+
+export const useComponentConfigStore = create<State & Action>((set) => ({
+  componentConfig: {
+    Container: {
+      name: 'Container',
+      defaultProps: {},
+      component: Container,
+    },
+    Button: {
+      name: 'Button',
+      defaultProps: {
+        type: 'primary',
+        text: '按钮',
+      },
+      component: Button,
+    },
+  },
+  registerComponent: (name, componentConfig) =>
+    set((state) => {
+      return {
+        ...state,
+        componentConfig: {
+          ...state.componentConfig,
+          [name]: componentConfig,
+        },
+      };
+    }),
+}));
