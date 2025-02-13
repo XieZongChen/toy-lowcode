9e82cbc482b496b684bdd04032aed32c88085991
XieZongChen <952937570@qq.com>
2025-02-13 10:31:12 +0800

    feat: 优化组件代码,提取公有逻辑

--- src/editor/materials/Page/index.tsx
+++ src/editor/materials/Page/index.tsx
@@ -1,35 +1,8 @@
-import { useDrop } from 'react-dnd';
 import { CommonComponentProps } from '@/editor/interface';
-import { useComponentConfigStore } from '@/editor/stores/component-config';
-import { useComponentsStore } from '@/editor/stores/components';
+import { useMaterialDrop } from '@/editor/hooks/useMaterialDrop';
 
 function Page({ id, name, children }: CommonComponentProps) {
-  const { addComponent } = useComponentsStore();
-  const { componentConfig } = useComponentConfigStore();
-
-  const [{ canDrop }, drop] = useDrop(() => ({
-    accept: ['Button', 'Container'],
-    drop: (item: { type: string }, monitor) => {
-      const didDrop = monitor.didDrop();
-      if (didDrop) {
-        // 防止组件嵌套的情况下重复触发 drop 事件
-        return;
-      }
-
-      const props = componentConfig[item.type].defaultProps;
-      addComponent(
-        {
-          id: new Date().getTime(),
-          name: item.type,
-          props,
-        },
-        id
-      );
-    },
-    collect: (monitor) => ({
-      canDrop: monitor.canDrop(),
-    }),
-  }));
+  const { canDrop, drop } = useMaterialDrop(['Button', 'Container'], id);
 
   return (
     <div
