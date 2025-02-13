4f8bb4daffcce6e5bb24ecd4f01eeddb5edc2cef
XieZongChen <952937570@qq.com>
2025-02-13 10:23:05 +0800

    feat: container 组件增加 drop

--- src/editor/materials/Container/index.tsx
+++ src/editor/materials/Container/index.tsx
@@ -1,8 +1,38 @@
-import { PropsWithChildren } from 'react';
+import { useDrop } from 'react-dnd';
+import { CommonComponentProps } from '@/editor/interface';
+import { useComponentConfigStore } from '@/editor/stores/component-config';
+import { useComponentsStore } from '@/editor/stores/components';
 
-const Container = ({ children }: PropsWithChildren) => {
+const Container = ({ id, children }: CommonComponentProps) => {
+  const { addComponent } = useComponentsStore();
+  const { componentConfig } = useComponentConfigStore();
+
+  const [{ canDrop }, drop] = useDrop(() => ({
+    accept: ['Button', 'Container'],
+    drop: (item: { type: string }) => {
+      const props = componentConfig[item.type].defaultProps;
+
+      addComponent(
+        {
+          id: new Date().getTime(),
+          name: item.type,
+          props,
+        },
+        id
+      );
+    },
+    collect: (monitor) => ({
+      canDrop: monitor.canDrop(),
+    }),
+  }));
+
   return (
-    <div className='border-[1px] border-[#000] min-h-[100px] p-[20px]'>
+    <div
+      ref={drop as unknown as (instance: HTMLDivElement | null) => void}
+      className={`min-h-[100px] p-[20px] ${
+        canDrop ? 'border-[2px] border-[blue]' : 'border-[1px] border-[#000]'
+      }`}
+    >
       {children}
     </div>
   );
