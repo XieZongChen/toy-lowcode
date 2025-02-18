53c49923f5d6378d461c88349f557ca39a8a3a58
XieZongChen <952937570@qq.com>
2025-02-18 10:35:27 +0800

    feat: 组件接入样式

--- src/editor/materials/Page/index.tsx
+++ src/editor/materials/Page/index.tsx
@@ -1,7 +1,7 @@
 import { CommonComponentProps } from '@/editor/interface';
 import { useMaterialDrop } from '@/editor/hooks/useMaterialDrop';
 
-function Page({ id, name, children }: CommonComponentProps) {
+function Page({ id, name, children, styles }: CommonComponentProps) {
   const { canDrop, drop } = useMaterialDrop(['Button', 'Container'], id);
 
   return (
@@ -9,7 +9,7 @@ function Page({ id, name, children }: CommonComponentProps) {
       data-component-id={id}
       ref={drop as unknown as (instance: HTMLDivElement | null) => void}
       className='p-[20px] h-[100%] box-border'
-      style={{ border: canDrop ? '2px solid blue' : 'none' }}
+      style={{ ...styles, border: canDrop ? '2px solid blue' : 'none' }}
     >
       {children}
     </div>
