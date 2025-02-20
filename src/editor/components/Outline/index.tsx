1294bf4345de064cac3338cfea2dd16700e1a7d0
XieZongChen <952937570@qq.com>
2025-02-20 10:34:00 +0800

    feat: 开发 Outline

--- src/editor/components/Outline/index.tsx
+++ src/editor/components/Outline/index.tsx
@@ -1,3 +1,19 @@
+import { Tree } from 'antd';
+import { useComponentsStore } from '@/editor/stores/components';
+
 export function Outline() {
-  return <div>Outline</div>;
+  const { components, setCurComponentId } = useComponentsStore();
+
+  return (
+    <Tree
+      fieldNames={{ title: 'desc', key: 'id' }}
+      treeData={components as any}
+      showLine
+      defaultExpandAll
+      onSelect={([selectedKey]) => {
+        // 选中时切换 CurComponentId 保证和页面中间区域联动
+        setCurComponentId(selectedKey as number);
+      }}
+    />
+  );
 }
