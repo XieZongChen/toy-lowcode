43afeaff824ceb6c05de03d1c73fcfd53706ecad
XieZongChen <952937570@qq.com>
2025-02-24 10:05:29 +0800

    feat: 优化 ComponentEvent

--- src/editor/components/Setting/ComponentEvent.tsx
+++ src/editor/components/Setting/ComponentEvent.tsx
@@ -1,58 +1,54 @@
-import { Collapse, Select, CollapseProps } from 'antd';
+import { useState } from 'react';
+import { Collapse, CollapseProps, Button } from 'antd';
 import { useComponentConfigStore } from '@/editor/stores/component-config';
+import type { ComponentEvent } from '@/editor/stores/component-config';
 import { useComponentsStore } from '@/editor/stores/components';
-import { GoToLink } from './actions/GoToLink';
-import { ShowMessage } from './actions/ShowMessage';
+import { ActionModal } from './ActionModal';
 
 export function ComponentEvent() {
-  const { curComponentId, curComponent, updateComponentProps } =
-    useComponentsStore();
+  const { curComponent } = useComponentsStore();
   const { componentConfig } = useComponentConfigStore();
+  const [actionModalOpen, setActionModalOpen] = useState(false);
+  const [curEvent, setCurEvent] = useState<ComponentEvent>();
 
   if (!curComponent) return null;
-
-  function selectAction(eventName: string, value: string) {
-    if (!curComponentId) return;
-
-    updateComponentProps(curComponentId, { [eventName]: { type: value } });
-  }
 
   const items: CollapseProps['items'] = (
     componentConfig[curComponent.name].events || []
   ).map((event) => {
     return {
       key: event.name,
-      label: event.label,
-      children: (
-        <div>
-          <div className='flex items-center'>
-            <div>动作：</div>
-            <Select
-              className='w-[160px]'
-              options={[
-                { label: '显示提示', value: 'showMessage' },
-                { label: '跳转链接', value: 'goToLink' },
-              ]}
-              onChange={(value) => {
-                selectAction(event.name, value);
-              }}
-              value={curComponent?.props?.[event.name]?.type}
-            />
-          </div>
-          {curComponent?.props?.[event.name]?.type === 'goToLink' && (
-            <GoToLink event={event} />
-          )}
-          {curComponent?.props?.[event.name]?.type === 'showMessage' && (
-            <ShowMessage event={event} />
-          )}
+      label: (
+        <div className='flex justify-between leading-[30px]'>
+          {event.label}
+          <Button
+            type='primary'
+            onClick={() => {
+              setCurEvent(event);
+              setActionModalOpen(true);
+            }}
+          >
+            添加动作
+          </Button>
         </div>
       ),
+      children: <div></div>,
     };
   });
 
   return (
     <div className='px-[10px]'>
       <Collapse className='mb-[10px]' items={items} />
+      <ActionModal
+        visible={actionModalOpen}
+        eventConfig={curEvent!}
+        handleOk={() => {
+          setActionModalOpen(false);
+        }}
+        handleCancel={() => {
+          setActionModalOpen(false);
+        }}
+      />
     </div>
   );
 }
