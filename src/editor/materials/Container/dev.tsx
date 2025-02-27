b8c8be46d91715fe58b1b267fd0dfc37a60ad83b
XieZongChen <952937570@qq.com>
2025-02-27 09:40:28 +0800

    feat: Container 支持画布拖拽

--- src/editor/materials/Container/dev.tsx
+++ src/editor/materials/Container/dev.tsx
@@ -1,13 +1,31 @@
+import { useEffect, useRef } from 'react';
+import { useDrag } from 'react-dnd';
 import { CommonComponentProps } from '@/editor/interface';
 import { useMaterialDrop } from '@/editor/hooks/useMaterialDrop';
 
-const Container = ({ id, children, styles }: CommonComponentProps) => {
+const Container = ({ id, name, children, styles }: CommonComponentProps) => {
+  const divRef = useRef<HTMLDivElement>(null);
+
   const { canDrop, drop } = useMaterialDrop(['Button', 'Container'], id);
+
+  const [_, drag] = useDrag({
+    type: name,
+    item: {
+      type: name,
+      dragType: 'move',
+      id: id,
+    },
+  });
+
+  useEffect(() => {
+    drop(divRef);
+    drag(divRef);
+  }, []);
 
   return (
     <div
       data-component-id={id}
-      ref={drop as unknown as (instance: HTMLDivElement | null) => void}
+      ref={divRef}
       className={`min-h-[100px] p-[20px] ${
         canDrop ? 'border-[2px] border-[blue]' : 'border-[1px] border-[#000]'
       }`}
