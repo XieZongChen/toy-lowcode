abf476c0b3c7e168f63c1dfaf0db0c2be5d89164
XieZongChen <952937570@qq.com>
2025-02-27 09:36:13 +0800

    feat: 画布区域的物料支持拖拽

--- src/editor/hooks/useMaterialDrop.ts
+++ src/editor/hooks/useMaterialDrop.ts
@@ -1,27 +1,41 @@
 import { useDrop } from "react-dnd";
-import { useComponentsStore } from "../stores/components";
+import { getComponentById, useComponentsStore } from "../stores/components";
 import { useComponentConfigStore } from "../stores/component-config";
 
+export interface ItemType {
+    type: string;
+    dragType?: 'move' | 'add',
+    id: number
+}
+
 export function useMaterialDrop(accept: string[], id: number) {
-    const { addComponent } = useComponentsStore();
+    const { components, addComponent, deleteComponent } = useComponentsStore();
     const { componentConfig } = useComponentConfigStore();
 
     const [{ canDrop }, drop] = useDrop(() => ({
         accept,
-        drop: (item: { type: string }, monitor) => {
+        drop: (item: ItemType, monitor) => {
             const didDrop = monitor.didDrop()
             if (didDrop) {
                 return;
             }
 
-            const config = componentConfig[item.type];
+            if (item.dragType === 'move') {
+                const component = getComponentById(item.id, components)!;
 
-            addComponent({
-                id: new Date().getTime(),
-                name: item.type,
-                props: config.defaultProps,
-                desc: config.desc
-            }, id)
+                deleteComponent(item.id);
+
+                addComponent(component, id)
+            } else {
+                const config = componentConfig[item.type];
+
+                addComponent({
+                    id: new Date().getTime(),
+                    name: item.type,
+                    desc: config.desc,
+                    props: config.defaultProps
+                }, id)
+            }
         },
         collect: (monitor) => ({
             canDrop: monitor.canDrop(),
--- src/editor/materials/Button/dev.tsx
+++ src/editor/materials/Button/dev.tsx
@@ -1,9 +1,25 @@
+import { Ref } from 'react';
 import { Button as AntdButton } from 'antd';
+import { useDrag } from 'react-dnd';
 import { CommonComponentProps } from '@/editor/interface';
 
 const Button = ({ id, type, text, styles }: CommonComponentProps) => {
+  const [_, drag] = useDrag({
+    type: 'Button',
+    item: {
+      type: 'Button',
+      dragType: 'move',
+      id: id,
+    },
+  });
+
   return (
-    <AntdButton data-component-id={id} type={type} style={styles}>
+    <AntdButton
+      ref={drag as unknown as Ref<HTMLButtonElement | HTMLAnchorElement>}
+      data-component-id={id}
+      type={type}
+      style={styles}
+    >
       {text}
     </AntdButton>
   );
